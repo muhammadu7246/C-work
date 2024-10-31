f09b68522cecde296c53dfe02651ddab918204d8
ameerhamza1123 <hamzamughal63420@gmail.com>
2024-10-31 16:43:07 +0500

    opps_work

---
+++ oppS_FIRST_CLASS/oppS_FIRST_CLASS/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace SimpleAdd
+{
+    class Calculator
+    {
+        // Addition
+        public void AddNumbers()
+        {
+            Console.Write("Addition:\n");
+            Console.Write("===========\n");
+            Console.Write("Enter the first number: ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the second number: ");
+
+            int num2 = int.Parse(Console.ReadLine());
+
+            int sum = num1 + num2;
+            Console.WriteLine($"The sum of {num1} and {num2} is {sum}.");
+        }
+
+        // Subtraction
+        public void SubtractNumbers()
+        {
+            Console.Write("Subtraction:\n");
+            Console.Write("===========\n");
+
+            Console.Write("Enter the first number: ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the second number: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            int difference = num1 - num2;
+            Console.WriteLine($"The difference of {num1} and {num2} is {difference}.");
+        }
+
+        // Multiplication
+        public void MultiplyNumbers()
+        {
+            Console.Write("Multiplication:\n");
+            Console.Write("===========\n");
+
+            Console.Write("Enter the first number: ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the second number: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            int product = num1 * num2;
+            Console.WriteLine($"The product of {num1} and {num2} is {product}.");
+        }
+
+        // Division
+        public void DivideNumbers()
+        {
+            Console.Write("division:\n");
+            Console.Write("===========\n");
+
+            Console.Write("Enter the first number: ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the second number: ");
+            int num2 = int.Parse(Console.ReadLine());
+            double quotient = (double)num1 / num2;
+            Console.WriteLine($"The quotient of {num1} and {num2} is {quotient}.");
+
+            
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator();
+
+            calculator.AddNumbers();
+            calculator.SubtractNumbers();
+            calculator.MultiplyNumbers();
+            calculator.DivideNumbers();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ oppS_FIRST_CLASS/oppS_FIRST_CLASS/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("oppS_FIRST_CLASS")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("oppS_FIRST_CLASS")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d9fb5aa9-5f14-40ad-89af-4d6952277566")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
