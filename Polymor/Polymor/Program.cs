4418fc00aa3200523e04808c3c7e2a1be94b3533
ameerhamza1123 <hamzamughal63420@gmail.com>
2024-11-12 16:33:02 +0500

    end

---
+++ Polymor/Polymor/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Polymor
+{
+    
+    class Creature
+    {
+        
+        public virtual void Speak()
+        {
+            Console.WriteLine("The creature makes an indistinct sound.");
+        }
+    }
+
+    
+    class Dog : Creature
+    {
+        
+        public override void Speak()
+        {
+            Console.WriteLine("Woof! Woof!");
+        }
+    }
+
+    
+    class Cat : Creature
+    {
+        
+        public override void Speak()
+        {
+            Console.WriteLine("Meow!");
+        }
+    }
+
+    
+    class Cow : Creature
+    {
+        
+        public override void Speak()
+        {
+            Console.WriteLine("Moo!");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Creature[] creatures = new Creature[]
+            {
+                new Creature(), 
+                new Dog(),       
+                new Cat(),       
+                new Cow()        
+            };
+
+            
+            foreach (var creature in creatures)
+            {
+                creature.Speak();
+            }
+
+            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ abstraction/abstraction/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Abstraction
+{
+    public abstract class Vehicle
+    {
+        public abstract void StartEngine();
+        public abstract void StopEngine();
+    }
+
+    public class Car : Vehicle
+    {
+        public override void StartEngine()
+        {
+            Console.WriteLine("The hourse is running");
+        }
+
+        public override void StopEngine()
+        {
+            Console.WriteLine("The hourse is dei");
+        }
+
+        public override string ToString()
+        {
+            return "This is a Simple Hourse Story";
+        }
+    }
+
+    public class Motorcycle : Vehicle
+    {
+        public override void StartEngine()
+        {
+            Console.WriteLine("The hourse is running on ground");
+        }
+
+        public override void StopEngine()
+        {
+            Console.WriteLine("The hourse is die after winning");
+        }
+
+        public override string ToString()
+        {
+            return "Simple Story";
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Vehicle myCar = new Car();
+            Console.WriteLine(myCar);
+            myCar.StartEngine();
+            myCar.StopEngine();
+
+            Vehicle myBike = new Motorcycle();
+            Console.WriteLine(myBike);
+            myBike.StartEngine();
+            myBike.StopEngine();
+
+            Console.ReadKey();
+        }
+    }
+}
