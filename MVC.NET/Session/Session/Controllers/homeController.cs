37c990c704e2ca7069370282223e3a1782de12fa
ameerhamza1123 <hamzamughal63420@gmail.com>
2024-12-17 16:44:14 +0500

    session

---
+++ MVC.NET/Session/Session/Controllers/homeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Session.Controllers
+{
+    public class homeController : Controller
+    {
+        // GET: home
+        public ActionResult Home()
+        {
+            ViewData["Var1"] = "Data comes from ViewData";
+            ViewBag.Var2 = "Data comes from ViewBag";
+            TempData["Var3"] = "Data comes from TempData";
+            Session["Var4"] = "Data comes from Session";
+
+            return View();
+        }
+        public ActionResult About()
+        {
+            TempData["var2"] = "Data comes from ViewBag";
+            return View();
+        }
+        public ActionResult Contect()
+        {
+            return View();
+        }
+        
+    }
+}
