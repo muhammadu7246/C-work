2b29ec1137d1f9fdc74fe0acb01fa764190e6fda
ameerhamza1123 <hamzamughal63420@gmail.com>
2024-12-19 16:03:12 +0500

    layout

---
+++ MVC.NET/layout/layout/Controllers/layoutController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace layout.Controllers
+{
+    public class layoutController : Controller
+    {
+        // GET: layout
+        public ActionResult Home()
+        {
+            return View();
+        }
+        public ActionResult about
+()
+        {
+            return View();
+        }
+    }
+}
