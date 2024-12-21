eb189468f1dabeffd4792f88a9ae0aefc007eaee
ameerhamza1123 <hamzamughal63420@gmail.com>
2024-12-21 16:17:23 +0500

    form

---
+++ MVC.NET/form/form/Controllers/formController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace form.Controllers
+{
+    public class FormController : Controller
+    {
+        // GET: form
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string username, string email, string password)
+        {
+            if (string.IsNullOrWhiteSpace(username))
+            {
+                ModelState.AddModelError("username", "User name is required");
+            }
+            if (string.IsNullOrWhiteSpace(email))
+            {
+                ModelState.AddModelError("email", "User email is required");
+            }
+            if (string.IsNullOrWhiteSpace(password))
+            {
+                ModelState.AddModelError("password", "User password is required");
+            }
+
+            if (ModelState.IsValid)
+            {
+                // Set success and form data messages
+                ViewData["SuccessMessage"] = "<script>alert('Thank you for submitting the form');</script>";
+                ViewData["nameMessage"] = username;
+                ViewData["emailMessage"] = email;
+                ViewData["PassMessage"] = password;
+
+                // Clear the model state after success
+                ModelState.Clear();
+            }
+
+            return View();
+        }
+    }
+}
