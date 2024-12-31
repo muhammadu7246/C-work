25e5f919efcd05c6811efa47a3d29ca389d12830
ameerhamza1123 <hamzamughal63420@gmail.com>
2024-12-31 16:50:39 +0500

    data

---
+++ MVC.NET/Api/Api/Controllers/ApiController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Api.Models;
+
+namespace Api.Controllers
+{
+    public class ApiController : Controller
+    {
+        // GET: Api
+        public JsonResult GetApi()
+        {
+            var Apis = new List<Api123>
+            {
+                 new Api123 { Id = 1, Name = "Laptop", Price = 750.00M },
+                new Api123 { Id = 2, Name = "Tablet", Price = 300.00M },
+                new Api123 { Id = 3, Name = "Smartphone", Price = 500.00M }
+            };
+            return Json(Apis, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
